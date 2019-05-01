bd6b54fcd696d8dcc65ae5731ccb686ed40b3efb
C Beach <cbeach@unicon.net>
2019-05-01 17:25:41 -0600

    #9 Allowed setting system properties to override the default email props

--- src/main/java/org/apereo/openequella/tools/toolbox/utils/GeneralUtils.java
+++ src/main/java/org/apereo/openequella/tools/toolbox/utils/GeneralUtils.java
@@ -1,8 +1,16 @@
 package org.apereo.openequella.tools.toolbox.utils;
 
+import java.util.Properties;
+
 public class GeneralUtils {
 
     public static boolean isNullOrEmpty(String s) {
         return s == null || s.isEmpty();
+    }
+
+    public static void setDefaultIfNotPresent(Properties p, String key, String val) {
+        if(!p.containsKey(key)) {
+            p.setProperty(key, val);
+        }
     }
 }
